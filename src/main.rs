6c21eb5dcec6f4f96b300c3eb45b3b86a595b819
Thomas Laferrière <t.laferriere@protonmail.com>
2025-11-23 10:50:07 -0500

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,11 @@
 /// This is the best implementation of this program.
 
 fn main() {
-    print_hello();
-    print_goodbye();
+    print("Hello, World!");
+    print("Goodbye, world!");
 }
 
-fn print_hello() {
-    println!("Hello, world!");
-}
-
-fn print_goodbye() {
-    println!("Goodbye, world!");
+// a function that prints a message
+fn print(m: &str) {
+    println!("{m}");
 }
